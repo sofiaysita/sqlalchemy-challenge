2dcfc1d07b854f9e3f120248d21f6ac40f81ca47
sofiaysita <sofiaysita3@gmail.com>
2022-11-08 16:52:07 -0600

    Sofia Ysita HW10

---
+++ SurfsUp/app.py
@@ -0,0 +1,75 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the Hawaii Climate API!<br/>"
+        f"The available Routes are:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # session = Session(engine)
+    # retrieve only the last 12 months of data
+    dates = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= dates).all()
+    session.close()
+    precipitation = {date: prcp for date, prcp in results}
+    return jsonify(precipitation)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # session = Session(engine)
+    results = session.query(Station.station).all()
+    session.close()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+@app.route("/api/v1.0/tobs")
+def temperature():
+    # session = Session(engine)
+    dates = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= dates).all()
+    session.close()
+    temperature = list(np.ravel(results))
+    return jsonify(temperature)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
