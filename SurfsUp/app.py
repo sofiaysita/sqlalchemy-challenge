e91247dcd701c02b0325c6079f09bbc1f8e73982
sofiaysita <sofiaysita3@gmail.com>
2022-11-08 20:52:03 -0600

    Code updated

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -38,7 +38,7 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/temp/start/end"
+        f"/api/v1.0/<start>/<end>"
     )
 
 
